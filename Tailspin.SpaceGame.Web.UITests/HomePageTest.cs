f9ba70608c40b8b80589a5084e9822d72165dc03
aghasd5dev <aghasd5dev@gmail.com>
2020-05-28 01:57:23 -0500

    Skipped IE Tests

--- Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
+++ Tailspin.SpaceGame.Web.UITests/HomePageTest.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Chrome.ChromeDriver;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.IE;
 using OpenQA.Selenium.Support.UI;
@@ -11,7 +12,7 @@ namespace UITests
 {
     [TestFixture("Chrome")]
     [TestFixture("Firefox")]
-    [TestFixture("IE")]
+    // [TestFixture("IE")]
     public class HomePageTest
     {
         private string browser;
@@ -41,15 +42,15 @@ namespace UITests
                   case "Firefox":
                     driver = new FirefoxDriver(cwd);
                     break;
-                  case "IE":
-                    driver = new InternetExplorerDriver(cwd);
-                    break;
+                //   case "IE":
+                //     driver = new InternetExplorerDriver(cwd);
+                //     break;
                   default:
                     throw new ArgumentException($"'{browser}': Unknown browser");
                 }
 
                 // Wait until the page is fully loaded on every page navigation or page reload.
-                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
 
                 // Navigate to the site.
                 // The site name is stored in the SITE_URL environment variable to make 
@@ -58,7 +59,7 @@ namespace UITests
                 driver.Navigate().GoToUrl(url + "/");
 
                 // Wait for the page to be completely loaded.
-                new WebDriverWait(driver, TimeSpan.FromSeconds(10))
+                new WebDriverWait(driver, TimeSpan.FromSeconds(5))
                     .Until(d => ((IJavaScriptExecutor) d)
                         .ExecuteScript("return document.readyState")
                         .Equals("complete"));
@@ -121,7 +122,7 @@ namespace UITests
             Assert.That(modalWasDisplayed, Is.True);
         }
 
-        private IWebElement FindElement(By locator, IWebElement parent = null, int timeoutSeconds = 10)
+        private IWebElement FindElement(By locator, IWebElement parent = null, int timeoutSeconds = 5)
         {
             // WebDriverWait enables us to wait for the specified condition to be true
             // within a given time period.
